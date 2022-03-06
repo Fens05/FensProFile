f6f1b3c99a404ee1f9f9e0773292de26b652c86d
Fens05 <fens05m@gmail.com>
2022-03-06 17:05:09 -0600

    reactstrap

--- fensprofile/src/App.js
+++ fensprofile/src/App.js
@@ -1,35 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import {BrowserRouter, Switch, Route} from 'router-dom'
-import BootstrapNavbar from './BootstrapNavbar.js'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 
 
-function App() {
-  return (
-  <div style={Background}>
-  
-    <BrowserRouter>
-      <div>
-    
-        <Headr 
-        to={to}
-        label={label}/>
-          </div>
-          <Switch>
-          <Route path="/" compoent={About} />
-          <Route path="/:projects" component={Projects} />
-          <Route path="/:contacts" component={Contact} />
-          </Switch>
-            
-        <div>
-        <Footer />
+  function App() {
+    return (
+        <div style={{
+            display: 'block', width: 700, padding: 30
+        }}>
+            <h4>ReactJS Reactstrap ListGroup Component</h4>
+            <ListGroup>
+                <ListGroupItem>Sample List Item 1</ListGroupItem>
+                <ListGroupItem>Sample List Item 2</ListGroupItem>
+                <ListGroupItem>Sample List Item 3</ListGroupItem>
+                <ListGroupItem>Sample List Item 4</ListGroupItem>
+                <ListGroupItem>Sample List Item 5</ListGroupItem>
+            </ListGroup>
         </div>
-    </BrowserRouter>
-      
-  </div>
-  );
+    );
 }
+//     <BrowserRouter>
+//       <div>
+    
+//         <Headr 
+//         to={to}
+//         label={label}/>
+//           </div>
+//           <Switch>
+//           <Route path="/" compoent={About} />
+//           <Route path="/:projects" component={Projects} />
+//           <Route path="/:contacts" component={Contact} />
+//           </Switch>
+            
+//         <div>
+//         <Footer />
+//         </div>
+//     </BrowserRouter>
+      
+//   </div>
+//   );
+// }
 
 export default App;
