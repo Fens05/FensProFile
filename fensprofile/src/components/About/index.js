41d7149b8d1ba47635c774d21237b51f715e501e
Fens05 <fens05m@gmail.com>
2022-03-11 21:30:35 -0600

    more

--- fensprofile/src/components/About/index.js
+++ fensprofile/src/components/About/index.js
@@ -5,8 +5,13 @@ import coverImage from '../../Assets/Images/about.jpg';
 function About() {
     return(
         <section className="my-5">
-            <h2 id="about">About Krista</h2>
+            <h2 id="about">Krista's Info</h2>
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+            <p>
+                Full-stack Web Developer from Minnesota. Has a background in Creative Writing, Scanning images,
+                and Research. I have a certificate from the University of Minnesota in Full Stack Web Desing. 
+                Where I learned HTML, CSS, JavaScript,SQL and React.
+            </p>
         </section>
 
     );
--- fensprofile/src/components/Navigation/index.js
+++ fensprofile/src/components/Navigation/index.js
@@ -20,11 +20,11 @@ function Navigation() {
                 <ul className="flex-row">
                     <li className="mx-2">
                         <a href="#about">
-                            About me
+                            About
                         </a>
                     </li>
                     <li className='my-2'>
-                        <a href="contact">
+                        <a href="#contact">
                          Contact</a>
                     </li>
                     <li className='mx-2'>
